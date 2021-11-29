4b47274f560354a34ab06146274c022d4744ee11
Bigman <89807949+Charlie-Es-Cool-and-Also-Guapo@users.noreply.github.com>
2021-11-29 17:21:49 -0500

    Intake System
    Done by Charlie and Carson.

--- src/main/java/frc/robot/subsystems/IntakeSystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSystem.java
@@ -1,5 +1,36 @@
 package frc.robot.subsystems;
 
-public class IntakeSystem {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
+public class IntakeSystem extends SubsystemBase{
+    private final double INTAKE_MOTOR_PERCENT = 0.65;
+
+    private WPI_TalonSRX intakeMotor;
+
+    public IntakeSystem () {
+        intakeMotor = new WPI_TalonSRX(11);
+
+        intakeMotor.setNeutralMode(NeutralMode.Brake);
+    }
+
+    public void intakeIn() {
+        intakeMotor.set(ControlMode.PercentOutput, INTAKE_MOTOR_PERCENT);
+    }
+
+    public void intakeOut() {
+        this.intakeMotor.set(ControlMode.PercentOutput, -INTAKE_MOTOR_PERCENT);
+    }
+    
+    public void intakeStop() {
+        this.intakeMotor.set(ControlMode.PercentOutput, 0);
+    }
+
+    @Override
+	public void periodic() {
+
+    }
     
 }
