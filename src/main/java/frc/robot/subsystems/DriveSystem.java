79f5feef419f2d87521600b07f7a79e6153f9b05
BreadStixFTW <77415101+BreadStixFTW@users.noreply.github.com>
2021-11-29 16:40:22 -0500

    Coded basic drive system code (Redo)

--- src/main/java/frc/robot/subsystems/DriveSystem.java
+++ src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -29,6 +29,8 @@ public class DriveSystem extends SubsystemBase {
     private static final double PEAK_OUTPUT = 1.0;
     private boolean turbo = false;
 
+    private static final int DEFAULT_TIMEOUT = 30;
+
     public DriveSystem() {
         leftMaster = new WPI_TalonSRX(0);
         rightMaster = new WPI_TalonSRX(1);
@@ -38,6 +40,40 @@ public class DriveSystem extends SubsystemBase {
         leftSlave.follow(leftMaster, FollowerType.AuxOutput1);
         rightSlave.follow(rightMaster, FollowerType.AuxOutput1);
 
+        leftMaster.setInverted(true);
+        leftSlave.setInverted(true);
+
+        leftMaster.setSensorPhase(true);
+        rightMaster.setSensorPhase(true);
+
+        leftMaster.configPeakOutputForward(PEAK_OUTPUT);
+		leftMaster.configPeakOutputReverse(-PEAK_OUTPUT);
+		leftSlave.configPeakOutputForward(PEAK_OUTPUT);
+		leftSlave.configPeakOutputReverse(-PEAK_OUTPUT);
+
+		rightMaster.configPeakOutputForward(PEAK_OUTPUT);
+		rightMaster.configPeakOutputReverse(-PEAK_OUTPUT);
+		rightSlave.configPeakOutputForward(PEAK_OUTPUT);
+		rightSlave.configPeakOutputReverse(-PEAK_OUTPUT);
+
+		leftMaster.configNominalOutputForward(0, DEFAULT_TIMEOUT);
+		leftMaster.configNominalOutputReverse(0, DEFAULT_TIMEOUT);
+		leftSlave.configNominalOutputForward(0, DEFAULT_TIMEOUT);
+		leftSlave.configNominalOutputReverse(0, DEFAULT_TIMEOUT);
+
+		rightMaster.configNominalOutputForward(0, DEFAULT_TIMEOUT);
+		rightMaster.configNominalOutputReverse(0, DEFAULT_TIMEOUT);
+		rightSlave.configNominalOutputForward(0, DEFAULT_TIMEOUT);
+		rightSlave.configNominalOutputReverse(0, DEFAULT_TIMEOUT);
+
+		leftMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, DEFAULT_TIMEOUT);
+        rightMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, DEFAULT_TIMEOUT);
         
+        rightMaster.setNeutralMode(NeutralMode.Brake);
+        leftMaster.setNeutralMode(NeutralMode.Brake);
+        rightSlave.setNeutralMode(NeutralMode.Brake);
+        leftSlave.setNeutralMode(NeutralMode.Brake);
+
+        navX = new AHRS(SPI.Port.kMXP);
     }
 }
