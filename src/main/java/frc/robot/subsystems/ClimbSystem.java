01e3f26f5001c434c52d71a59d48f1c6b109796c
BreadStixFTW <77415101+BreadStixFTW@users.noreply.github.com>
2021-11-29 17:03:38 -0500

    Coded ClimbSystem

--- src/main/java/frc/robot/subsystems/ClimbSystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSystem.java
@@ -1,6 +1,15 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ClimbSystem {
-    
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
+public class ClimbSystem extends SubsystemBase {
+    private WPI_TalonSRX hookMotor;
+    private WPI_TalonSRX climbMotor;
+
+    private static final double HOOK_PERCENT_OUTPUT = 0.9;
+    private static final double HOOK_DOWN_PERCENT_OUTPUT = 0.4;
 }
