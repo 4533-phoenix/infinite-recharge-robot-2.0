5dcb98ad65a81c511c2823846caed5b68a25d731
Carson-walker <carson.walker330@gmail.com>
2021-12-06 17:02:04 -0500

    Update ShooterFactory.java
    started shooter factory yass

--- src/main/java/frc/robot/commands/ShooterCommands/ShooterFactory.java
+++ src/main/java/frc/robot/commands/ShooterCommands/ShooterFactory.java
@@ -1,5 +1,33 @@
 package frc.robot.commands.ShooterCommands;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Robot;
+
 public class ShooterFactory {
-    
+    public static Command flywheelOutCommand() {
+        return new InstantCommand(
+            () -> Robot.shooter.flywheelOut(),
+            Robot.shooter
+        );
+    }
+    public static Command flywheelInCommand() {
+        return new InstantCommand(
+            () -> Robot.shooter.flywheelIn(),
+            Robot.shooter
+        );
+    }
+    public static Command flywheelStopCommand() {
+        return new InstantCommand(
+            () -> Robot.shooter.flywheelStop(),
+            Robot.shooter
+        );
+    }
+    public static Command flywheelAndIntakeOutCommand() {
+        return new InstantCommand(
+            () -> Robot.shooter.flywheelAndIntakeStop(),
+            Robot.shooter
+        );
+    }
 }
+
